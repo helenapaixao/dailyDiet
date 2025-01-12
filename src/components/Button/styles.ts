0bc588bbc9fcc2a859feb6f97839af59ae604ab0
helenapaixao <hp.helenapaixao@gmail.com>
2025-01-12 17:37:13 -0300

    style: Update Button component styles for consistent padding and width

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,20 +2,18 @@ import styled from 'styled-components/native'
 import { ButtonProps } from '.'
 
 export const ButtonContainer = styled.TouchableOpacity<ButtonProps>`
-  width: 100%;
   flex-direction: row;
   height: 50px;
-  border-radius: 6px;
   border-color: ${({ type }) =>
     type === 'primary'
       ? '#333638'
       : type === 'secondary'
       ? '#333638'
       : '#333638'};
-  border: 1px;
-  width: ${({ size }) =>
+  border-radius: 6px;
+    width: ${({ size }) =>
     size === 's' ? '191px' : size === 'm' ? '327px' : '100%'};
-  padding: 0 12px;
+  padding: 0 16px;
   background-color: ${({ type }) =>
     type === 'primary' ? '#333638' : type === 'secondary' ? '#FFFF' : '#FFF'};
   justify-content: center;
@@ -24,7 +22,6 @@ export const ButtonContainer = styled.TouchableOpacity<ButtonProps>`
 
 export const Title = styled.Text<ButtonProps>`
   font-size: 14px;
-  flex: 1;
   color: ${({ type }) =>
     type === 'primary'
       ? '#FFFF'
@@ -36,6 +33,5 @@ export const Title = styled.Text<ButtonProps>`
 `
 
 export const ContentIcon = styled.View`
-  height: 18px;
-  text-align: center;
+  margin-right: 8px;
 `
