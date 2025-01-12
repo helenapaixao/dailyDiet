04ce402d1fb05618c2022089277a53ff95a46ae5
helenapaixao <hp.helenapaixao@gmail.com>
2025-01-12 17:40:20 -0300

    style: Update Statistics screen title color and margin

--- src/screens/Statistics/styles.ts
+++ src/screens/Statistics/styles.ts
@@ -1,52 +1,52 @@
-import styled from 'styled-components/native'
+import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  background-color: #e5f0db;
   flex: 1;
-`
+  background-color: #e5f0db;
+`;
 
 export const Header = styled.View`
   background-color: #e5f0db;
-  width: 100%;
   align-items: center;
   justify-content: center;
-`
+  padding: 24px;
+`;
+
 export const TitlePercent = styled.Text`
   font-family: 'Nunito_700Bold';
   font-size: 32px;
   color: #1b1d1e;
   font-weight: bold;
-`
+`;
 
 export const TextContent = styled.Text`
   font-size: 14px;
   color: #333638;
   font-family: 'Nunito_400Regular';
-`
+  text-align: center;
+`;
 
 export const Content = styled.View`
   background-color: #fff;
   border-top-left-radius: 20px;
   border-top-right-radius: 20px;
-  position: absolute;
-  padding: 16px;
-  z-index: 1;
-  justify-content: center;
+  padding: 24px;
+  margin-top: -16px;
+  flex: 1;
   align-items: center;
-  margin-top: 100px;
-  width: 100%;
-  height: 85%;
-  justify-content: flex-start;
-`
+`;
 
 export const Title = styled.Text`
   font-size: 14px;
   font-weight: bold;
-  color: #1B1D1E;
-  margin-bottom: 23px;
-
-`
+  color: #1b1d1e;
+  font-family: 'Nunito_700Bold';
+  margin-bottom: 24px;
+`;
 
 export const ContainerFooter = styled.View`
   flex-direction: row;
-`
+  justify-content: space-between;
+  width: 100%;
+  margin-top: 16px;
+`;
