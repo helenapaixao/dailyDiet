cb4f8f7cd9946c14a7e00dfdc98021759753b66a
helenapaixao <hp.helenapaixao@gmail.com>
2025-01-13 12:18:51 -0300

    refactor: Remove unused DateInput component and related files

--- src/components/Input/Date/interface.ts
+++
@@ -1,8 +0,0 @@
-export type InputDateTypeStyleProps = 'background-white' | 'background-grey';
-
-export interface DateInputProps {
-  children: React.ReactNode;
-  focused: boolean;
-  type?: InputDateTypeStyleProps;
-  onPress: () => void;
-}
